662969307adf052e0d66981a52143e3524f1b707
BeachedCode <Beached@mail.uc.edu>
2025-02-28 09:33:36 -0500

    Add project files.

---
+++ URISignIn/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace URISignIn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

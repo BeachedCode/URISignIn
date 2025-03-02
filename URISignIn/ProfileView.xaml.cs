52f2c7519c744358cd72e32acb45f58447b517fe
Ethan <Beached@mail.uc.edu>
2025-03-02 10:14:49 -0500

    Added ProfileView and Added Display Message

--- URISignIn/ProfileView.xaml.cs
+++ URISignIn/ProfileView.xaml.cs
@@ -1,9 +1,22 @@
 namespace URISignIn;
+[QueryProperty(nameof(User_Name),"username")]
+[QueryProperty(nameof(User_Email),"email")]
 
 public partial class ProfileView : ContentPage
 {
+	public string User_Name { get; set; }
+	public string User_Email { get; set; }
 	public ProfileView()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        DisplayAlert("Success", "You have successfully signed up. \n" +
+            "Username: " + User_Name + "\n" +
+            "Email: " + User_Email, "OK");
+    }
 }
